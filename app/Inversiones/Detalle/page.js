0d770fee4e69a34c2a22f543fcdf7f1ce041f1eb
jem48878 <estebanjaime01@yahoo.com.ar>
2025-06-20 14:10:27 -0300

    detalleClient

--- app/Inversiones/Detalle/page.js
+++ app/Inversiones/Detalle/page.js
@@ -1,35 +1,15 @@
-"use client";
-import Link          from 'next/link';
-import MyStyles      from  '@/components/MyStyles.css';    
 import React from 'react';
-import { ContextProvider } from '@/contexts/MyContext';
-import { useSearchParams } from 'next/navigation';
+ 
+import DetalleClient from '@/components/Inversiones/DetalleComponents//DetalleClient';
 
-import Encabezado from '@/components/Inversiones/PageComponents/Encabezado';
-
-import {Suspense} from 'react' ;
-import MostrarDetalle from '@/components/Inversiones/DetalleComponents/MostrarDetalleInversion';
-
-
-
-function Home() { 
-  const searchParams = useSearchParams();
-  const owner = searchParams.get('owner');
-  const title = searchParams.get('title');        
-  
-  return (
-    <ContextProvider>   
-    <Encabezado text="Encabezado"/> 
-
-    <div className='body'>      
-        <MostrarDetalle owner={owner} title={title} />  
-        <br/>
-    </div>
-    </ContextProvider>  
+function Home() {   
+  return (     
+        <DetalleClient />  
   );
 }
 
 export default Home;
+
 
 
     
