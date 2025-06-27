955f7093806ca8faf36d91b473127b4597e70066
jem48878 <estebanjaime01@yahoo.com.ar>
2025-06-27 11:45:04 -0300

    etapa2

---
+++ app/Access/Login/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import Login from '@/components/Access/Login';
+
+function LoginPage() {
+  
+  return (      
+   <>
+     <Login />
+   </>
+  )
+}
+
+
+export default LoginPage;
---
+++ utils/dataUsuariosFireBase.js
@@ -0,0 +1,54 @@
+const FIREBASE_USUARIOS_URL = 'https://databuzzconfrontnx-default-rtdb.firebaseio.com/dataUsuario.json';
+
+
+// Leer todas los usuarios desde Firebase
+export async function loadUsuario() {
+  try {
+    console.log("--load usuario firebase--")  
+    const res = await fetch(FIREBASE_USUARIOS_URL);
+    const data = await res.json();
+    // Convertir el objeto a array
+    if (!data) return [];
+    return Object.entries(data).map(([id, item]) => ({ id, ...item }));
+  } catch (error) {
+    console.error('Error al leer usuarios de Firebase:', error);
+    return [];
+  }
+}
+
+
+// Agregar 
+export async function addUsuario(entrada) {
+  try {
+    const res = await fetch(FIREBASE_USUARIOS_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(entrada),
+    });
+    const result = await res.json();
+    return result; // { name: "-idGenerado" }
+  } catch (error) {
+    console.error('Error al agregar usuario:', error);
+  }
+}
+
+
+
+// Modificar un usuario existente
+export async function updateInversion(id, nuevosDatos) {
+  try {
+      
+    const url = FIREBASE_USUARIOS_URL.replace('.json', `/${id}.json`);
+      
+      
+    const res = await fetch(url, {
+      method: 'PATCH', // o PUT si querés reemplazar completamente
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(nuevosDatos),
+    });
+    const result = await res.json();
+    return result;
+  } catch (error) {
+    console.error('Error al modificar usuario en Firebase:', error);
+  }
+}
