27b8e2887fd9eb421cd8c9fa58c24fe3d5d1d573
jem48878 <estebanjaime01@yahoo.com.ar>
2025-07-05 12:47:55 -0300

    prueba api

--- utils/dataUsuariosFunction.js
+++ utils/dataUsuariosFunction.js
@@ -607,7 +607,7 @@ import { database , auth }  from '@/src/firebase';
 export async function crearCuenta(entrada) {
     const entorno = process.env.NEXT_PUBLIC_ENTORNO;     
     
-    const opcion = 1 
+    const opcion = 2 
     if ( opcion == 1 ) 
         return await crearCuenta1(entrada)
     else {
